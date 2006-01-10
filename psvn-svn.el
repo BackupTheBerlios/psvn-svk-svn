0f1c6deb1a464affa77bd68fea18cf30a9f42b7b
intrigeri <intrigeri@64fd5ddf-060a-0410-9f93-958c19a91c19>
2006-01-10 20:31:49 +0000

    r636@crapouille:  intrigeri | 2006-01-08 02:11:42 +0100  added psvn-svn.el skel

---
+++ psvn-svn.el
@@ -0,0 +1,30 @@
+;;; psvn-svn.el --- Subversion support for psvn.el / Emacs
+;; Copyright (C) 2002-2005 by Stefan Reichoer & intrigeri
+
+;; Authors:
+;;   - Stefan Reichoer, <stefan@xsteve.at>
+;;   - Ben Voui, <intrigeri@boum.org>
+;; $Id$
+;; $URL$
+
+;; psvn-svn.el is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; psvn-svn.el is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+(provide 'psvn-svn)
+
+;; Local Variables:
+;; indent-tabs-mode: nil
+;; End:
+;;; psvn-svn.el ends here
